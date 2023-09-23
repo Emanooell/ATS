5c3b3387dde30d5f231d2b414edd7500ce1b706f
Emanoel Rodrigues <emanuellegario@gmail.com>
2023-09-23 15:44:51 -0300

    projeto finalizado!

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const imagem = document.getElementById('imagem')
+document.addEventListener('DOMContentLoaded', () => {
+    const imagem = document.getElementById('imagem-rotacao');
 
-imagem.addEventListener('mouseover', () =>{
+    imagem.addEventListener('mouseover', () => {
+        imagem.classList.add('rotacao');
+    });
+
+    imagem.addEventListener('mouseout', () => {
+        imagem.classList.remove('rotacao');
+    });
+
+    const formulario = document.getElementById('formulario')
+formulario.addEventListener('submit', (evento) =>{
+    evento.preventDefault()
+    formulario.reset()
     
-    imagem.classList.add('rotacao')
 })
-imagem.addEventListener('mouseout', () =>{
-    imagem.classList.remove('rotacao')
-})
+});
+
