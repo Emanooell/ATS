708f42faa9300631ba7881b1aa1142b67381d683
Emanoel Rodrigues <emanuellegario@gmail.com>
2023-10-11 20:48:38 -0300

    Projeto finalizado!

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('formulario');
     const botaoEnviar = document.getElementById('botaoEnviar');
 
-    botaoEnviar.addEventListener('click', () => {
+    botaoEnviar.addEventListener('click', (event) => {
+        event.preventDefault(); 
+
         const nome = document.getElementById('nome').value;
         const email = document.getElementById('email').value;
+
+        formulario.reset();
+
+        botaoEnviar.value = 'Enviado';
 
         function sendEmail(nome, email) {
             const dadosEmail = {
                 destinatario: email,
                 assunto: 'Bem vindo a ATS',
-                corpo: `Olá, ${nome}, bem vindo a ATS. Em breve entraremos em contato com você!`,
+                corpo: `Olá, ${nome}, Obrigado por ter encontrado conosco. Nós da equipe ATS será um prazer poder ajudá-lo! Conte mais sobre o que você deseja e suas ideias!`,
             };
 
             fetch('http://localhost:3000/enviar-email', {
@@ -19,16 +25,23 @@ document.addEventListener('DOMContentLoaded', () => {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(dadosEmail),
-                mode: 'cors',  // Adicione esta linha para lidar com CORS
+                mode: 'cors',  
             })
             .then(response => response.text())
-            .then(data => console.log(data))
-            .catch(error => console.error('Erro:', error));
-
-            console.log("O formulário foi submetido!");
+            .then(data => {
+                console.log(data);
+                alert('E-mail enviado, em breve vamos entrar em contato com você. Caso o e-mail não chegue, tente preencher o formulário novamente!');
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+                alert('Erro ao enviar o e-mail. Por favor, tente novamente mais tarde.');
+            });
         }
 
-        // Chame a função sendEmail com os dados do formulário
         sendEmail(nome, email);
+
+        setTimeout(() => {
+            botaoEnviar.value = 'Enviar';
+        }, 1000); 
     });
 });
